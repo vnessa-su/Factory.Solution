9c999b59008d2261bf5be43c7f465824e45b1ec4
Vanessa Su <vnessa.su@gmail.com>
2021-05-28 14:51:00 -0400

    :hammer: Add CRUD routes to Engineers

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -7,55 +7,94 @@ using System.Linq;
 
 namespace Factory.Controllers
 {
-  public class ModelsController : Controller
+  public class EngineersController : Controller
   {
-    private readonly TemplateContext _db;
+    private readonly FactoryContext _db;
 
-    public ModelsController(TemplateContext db)
+    public EngineersController(FactoryContext db)
     {
       _db = db;
     }
 
     public ActionResult Index()
     {
-      return View();
+      List<Engineer> allEngineers = _db.Engineers
+        .OrderBy(engineer => engineer.LastName)
+        .ThenBy(engineer => engineer.FirstName)
+        .ToList();
+      return View(allEngineers);
     }
 
     public ActionResult Create()
     {
+      ViewBag.CourseId = new SelectList(_db.Machines, "MachineId", "ProductModel");
       return View();
     }
 
     [HttpPost]
-    public ActionResult Create()
+    public ActionResult Create(Engineer engineer, int[] MachineIds)
     {
+      foreach (int machineId in MachineIds)
+      {
+        bool entryExists = _db.EngineerMachine
+          .Any(entry => entry.EngineerId == engineer.EngineerId && entry.MachineId == machineId);
+        if (!entryExists)
+        {
+          _db.EngineerMachine.Add(new EngineerMachine() { MachineId = machineId, EngineerId = engineer.EngineerId });
+        }
+      }
+      _db.Engineers.Add(engineer);
+      _db.SaveChanges();
       return RedirectToAction("Index");
     }
 
     public ActionResult Details(int id)
     {
-      return View();
+      Engineer selectedEngineer = _db.Engineers
+        .Include(engineer => engineer.EngineerMachineJoinEntities)
+        .ThenInclude(join => join.Machine)
+        .FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(selectedEngineer);
     }
 
     public ActionResult Edit(int id)
     {
-      return View();
+      Engineer selectedEngineer = _db.Engineers
+        .Include(engineer => engineer.EngineerMachineJoinEntities)
+        .ThenInclude(join => join.Machine)
+        .FirstOrDefault(engineer => engineer.EngineerId == id);
+      ViewBag.CourseId = new SelectList(_db.Machines, "MachineId", "ProductModel");
+      return View(selectedEngineer);
     }
 
     [HttpPost]
-    public ActionResult Edit()
+    public ActionResult Edit(Engineer engineer, int[] MachineIds)
     {
-      return RedirectToAction("Index");
+      foreach (int machineId in MachineIds)
+      {
+        EngineerMachine joinEntry = _db.EngineerMachine
+          .Where(entry => entry.EngineerId == engineer.EngineerId && entry.MachineId == machineId)
+          .Single();
+        _db.EngineerMachine.Remove(joinEntry);
+      }
+
+      _db.Entry(engineer).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = engineer.EngineerId });
     }
 
     public ActionResult Delete(int id)
     {
-      return View();
+      Engineer selectedEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(selectedEngineer);
     }
 
     [HttpPost, ActionName("Delete")]
     public ActionResult DeleteConfirmed(int id)
     {
+      Engineer selectedEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      _db.Engineers.Remove(selectedEngineer);
+      _db.SaveChanges();
       return RedirectToAction("Index");
     }
   }
