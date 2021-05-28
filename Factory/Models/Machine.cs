65f00dc9d2465d4ff322d592d7158e22e3b1f75c
Vanessa Su <vnessa.su@gmail.com>
2021-05-28 13:37:16 -0400

    :hammer: Add properties to Machine

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,7 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Factory.Models
 {
-  public class Model
+  public class Machine
   {
-    //properties
+    public int MachineId { get; set; }
+
+    [Column(TypeName = "VARCHAR(255)")]
+    public string Manufacturer { get; set; }
+
+    [Display(Name = "Model")]
+    [Column(TypeName = "VARCHAR(255)")]
+    public string ProductModel { get; set; }
+
+    [Display(Name = "Address")]
+    [Column(TypeName = "VARCHAR(255)")]
+    public string CompanyAddress { get; set; }
+
+    [Display(Name = "Phone Number")]
+    [Column(TypeName = "VARCHAR(20)")]
+    public string CompanyPhoneNumber { get; set; }
+
+    [Display(Name = "Email")]
+    [Column(TypeName = "VARCHAR(255)")]
+    public string CompanyEmail { get; set; }
+
+    [Display(Name = "Website")]
+    [Column(TypeName = "VARCHAR(255)")]
+    public string CompanyWebsite { get; set; }
+
+    public virtual ICollection<EngineerMachine> EngineerMachineJoinEntities { get; }
+
+    public Machine()
+    {
+      this.EngineerMachineJoinEntities = new HashSet<EngineerMachine>();
+    }
   }
 }
