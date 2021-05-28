5c01cab01a97a115a6f52b2f428d82eefd63add5
Vanessa Su <vnessa.su@gmail.com>
2021-05-28 17:36:13 -0400

    :hammer: Add database access to get lists for Home Index

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,15 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Factory.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
     {
-
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
-
+      _db = db;
     }
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      List<Engineer> allEngineers = _db.Engineers
+        .OrderBy(engineer => engineer.LastName)
+        .ThenBy(engineer => engineer.FirstName)
+        .ToList();
+      ViewBag.EngineerList = allEngineers;
+
+      List<Machine> allMachines = _db.Machines
+        .OrderBy(machine => machine.Manufacturer)
+        .ThenBy(machine => machine.ProductModel)
+        .ToList();
+      ViewBag.MachineList = allMachines;
+      return View();
+    }
+
+  }
 }
