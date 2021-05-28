a6fbc5456033605e3e721f570c253b1ed8a6d1ed
Vanessa Su <vnessa.su@gmail.com>
2021-05-28 15:10:48 -0400

    :card_file_box: Generate initial database migration files

---
+++ Factory/Migrations/20210528185921_Initial.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Engineers",
+                columns: table => new
+                {
+                    EngineerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(type: "VARCHAR(50)", nullable: true),
+                    LastName = table.Column<string>(type: "VARCHAR(50)", nullable: true),
+                    Address = table.Column<string>(type: "VARCHAR(255)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "VARCHAR(20)", nullable: true),
+                    Email = table.Column<string>(type: "VARCHAR(255)", nullable: true),
+                    HireDate = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Engineers", x => x.EngineerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Machines",
+                columns: table => new
+                {
+                    MachineId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Manufacturer = table.Column<string>(type: "VARCHAR(255)", nullable: true),
+                    ProductModel = table.Column<string>(type: "VARCHAR(255)", nullable: true),
+                    CompanyAddress = table.Column<string>(type: "VARCHAR(255)", nullable: true),
+                    CompanyPhoneNumber = table.Column<string>(type: "VARCHAR(20)", nullable: true),
+                    CompanyEmail = table.Column<string>(type: "VARCHAR(255)", nullable: true),
+                    CompanyWebsite = table.Column<string>(type: "VARCHAR(255)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Machines", x => x.MachineId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EngineerMachine",
+                columns: table => new
+                {
+                    EngineerMachineId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    EngineerId = table.Column<int>(type: "int", nullable: false),
+                    MachineId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EngineerMachine", x => x.EngineerMachineId);
+                    table.ForeignKey(
+                        name: "FK_EngineerMachine_Engineers_EngineerId",
+                        column: x => x.EngineerId,
+                        principalTable: "Engineers",
+                        principalColumn: "EngineerId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EngineerMachine_Machines_MachineId",
+                        column: x => x.MachineId,
+                        principalTable: "Machines",
+                        principalColumn: "MachineId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EngineerMachine_EngineerId",
+                table: "EngineerMachine",
+                column: "EngineerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EngineerMachine_MachineId",
+                table: "EngineerMachine",
+                column: "MachineId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EngineerMachine");
+
+            migrationBuilder.DropTable(
+                name: "Engineers");
+
+            migrationBuilder.DropTable(
+                name: "Machines");
+        }
+    }
+}
