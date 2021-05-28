f02f73ccb462b2b2b27ba320c8a8a095ff1442e7
Vanessa Su <vnessa.su@gmail.com>
2021-05-28 12:28:41 -0400

    :hammer: Add properties to Engineer

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,7 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace Factory.Models
 {
-  public class Model
+  public class Engineer
   {
-    //properties
+    public int EngineerId { get; set; }
+
+    [Display(Name = "First Name")]
+    [Column(TypeName = "VARCHAR(50)")]
+    public string FirstName { get; set; }
+
+    [Display(Name = "Last Name")]
+    [Column(TypeName = "VARCHAR(50)")]
+    public string LastName { get; set; }
+
+    [Column(TypeName = "VARCHAR(255)")]
+    public string Address { get; set; }
+
+    [Display(Name = "Phone Number")]
+    [Column(TypeName = "VARCHAR(20)")]
+    public string PhoneNumber { get; set; }
+
+    [Column(TypeName = "VARCHAR(255)")]
+    public string Email { get; set; }
+
+    [Display(Name = "Hire Date")]
+    [DataType(DataType.Date)]
+    [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+    public DateTime HireDate { get; set; } = DateTime.Now;
+
+    public virtual ICollection<EngineerMachine> EngineerMachineJoinEntities { get; }
+
+    public Engineer()
+    {
+      this.EngineerMachineJoinEntities = new HashSet<EngineerMachine>();
+    }
   }
 }
