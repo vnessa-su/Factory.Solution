a0901c3f7377ff5c4a2ed4d025afe6d470afb422
Vanessa Su <vnessa.su@gmail.com>
2021-05-28 13:40:34 -0400

    :hammer: Add properties to EngineerMachine

--- Factory/Models/EngineerMachine.cs
+++ Factory/Models/EngineerMachine.cs
@@ -1,7 +1,11 @@
 namespace Factory.Models
 {
-  public class Model
+  public class EngineerMachine
   {
-    //properties
+    public int EngineerMachineId { get; set; }
+    public int EngineerId { get; set; }
+    public int MachineId { get; set; }
+    public virtual Engineer Engineer { get; set; }
+    public virtual Machine Machine { get; set; }
   }
 }
